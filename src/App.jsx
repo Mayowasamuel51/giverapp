0089d77f8e2b18d5c5f594928884979790e37e91
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-08-01 08:54:20 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar' // calling the navbar function
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      
+      </div>
+  )
+} 
+export default App
---
+++ src/components/Section.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Section() {
+    return (  
+        <div>
+            <h1>THIS IS Section </h1>
+        </div>
+    )
+}
+
+export default Section;
